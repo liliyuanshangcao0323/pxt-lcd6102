931e7aad38ab4e610972855ae196393c0d4b5970
liliyuanshangcao0323 <3884006568@qq.com>
2026-01-22 21:31:13 +0800

    Add files via upload

---
+++ blocks.ts
@@ -0,0 +1,10 @@
+//% color="#1f77d4"
+namespace lcd1602 {
+    /**
+     * Initialize LCD
+     */
+    //% block="Initialize LCD"
+    export function init(): void {
+        basic.showString("LCD Init");
+    }
+}
